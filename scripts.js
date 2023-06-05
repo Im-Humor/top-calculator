fb69a188605859ef288f6fd60b2b75f0a45fdaa3
Im-Humor <orangite@gmail.com>
2023-06-04 22:30:42 -0700

    remove <p> elements from key divs to eliminate complexity. complete basic mathematical functions and operate function.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,40 @@
-function add() {
-    
+function add(num1, num2) {
+    return num1 + num2;
 }
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+function operate(operator, num1, num2) {
+    newVal = 0;
+    switch (operator) {
+        case "+":
+            newVal = add(num1, num2);
+            break;
+        case "-":
+            newVal = subtract(num1, num2);
+            break;
+        case "/":
+            newVal = divide(num1, num2);
+            break;
+        case "*":
+            newVal = multiply(num1, num2);
+            break;
+    }
+    return newVal;
+}
+
+numChoice1 = 0;
+numChoice2 = 0;
+opChoice = "";
+displayVal = 0;
+
