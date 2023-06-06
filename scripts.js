f2b84b300306449ece0697430efdcbaa97432c48
Im-Humor <orangite@gmail.com>
2023-06-05 19:28:50 -0700

    refactor codebase for variable consistency, add 'calculate-at-operand' functionality, and set a height for the 'screen'

--- scripts.js
+++ scripts.js
@@ -11,7 +11,13 @@ function multiply(num1, num2) {
 }
 
 function divide(num1, num2) {
-    return Number(num1) / Number(num2);
+    if (Number(num2) == 0) {
+        return "Can't divide by 0"
+    }
+    else {
+        return Number(num1) / Number(num2);
+    }
+
 }
 
 function operate(operator, num1, num2) {
@@ -34,52 +40,58 @@ function operate(operator, num1, num2) {
 }
 
 let numChoice1 = 0;
-let operatedVal = 0;
 let opChoice = "";
 let displayVal = "";
+const keyArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."]
+const fnArray = ["power", "clear", "%", "+/-"]
+const opArray = ["+", "-", "/", "x"]
 let buttonHistory = [];
-const keyArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ".", "+", "-", "/", "x"]
+
+
 const display = document.querySelector("#display-frame");
 
 
 // add event listeners to all buttons
 const buttons = document.querySelectorAll('button');
 buttons.forEach(button => button.addEventListener("click", () => {
+    const buttonText = button.textContent;
+    buttonHistory.push(buttonText);
     // if equal sign is pressed, calculate operator based on last operand clicked
     // but only if an operand has been selected at least once
     // then update display and return
-    if (Number.isFinite(Number(buttonHistory.at(-1))) && opChoice != "" && button.textContent == "=") {
-        operatedVal = operate(opChoice, Number(numChoice1), Number(displayVal));
-        displayVal = operatedVal;
-        display.textContent = displayVal;
-        numChoice1 = Number(buttonHistory.at(-1));
+    if (buttonText == "=" && opChoice != "") {
+        numChoice1 = operate(opChoice, Number(numChoice1), Number(displayVal));
+        displayVal = numChoice1;
+        display.textContent = numChoice1;
         return;
     }
     // if cleared, reset values
-    else if (button.textContent == "clear") {
-        displayVal = 0;
+    else if (buttonText == "clear") {
+        displayVal = "";
         numChoice1 = 0;
         opChoice = "";
-        buttonHistory = [];
-        operatedVal = 0;
-    }
-    // add key press to list of key presses
-    if (keyArray.includes(button.textContent)) {
-        buttonHistory.push(button.textContent);
     }
     // if user presses a number after an operand,
     // the display value is set to the new number
-    if (Number.isFinite(Number(buttonHistory.at(-1))) && opChoice != "" && buttonHistory.at(-2) == opChoice && button.textContent != "=") {
-        displayVal = button.textContent;
+    if ((Number.isFinite(Number(buttonText)) && opArray.includes(buttonHistory.at(-2))) || display.textContent == "") {
+        displayVal = buttonText;
     }
     // if the last key pressed was a number or decimal,
     // append new key to display value
-    else if (Number.isFinite(Number(buttonHistory.at(-1))) || (buttonHistory.at(-1) == "." && displayVal % 1 == 0)) {
-        displayVal += buttonHistory.at(-1);
+    else if (Number.isFinite(Number(buttonText)) || (buttonText == "." && displayVal % 1 == 0)) {
+        displayVal += buttonText;
     }
-    else if ((keyArray.includes(buttonHistory.at(-1), 11))) {
-        numChoice1 = displayVal;
-        opChoice = buttonHistory.at(-1);
+    // set operand and save display number
+    else if ((opArray.includes(buttonText))) {
+        if (opChoice == "") {
+            numChoice1 = displayVal;
+            opChoice = buttonText;
+        }
+        else {
+            displayVal = operate(opChoice, Number(numChoice1), Number(displayVal));
+            numChoice1 = displayVal;
+            opChoice = buttonText;
+        }
     }
     // update display if not calculating anything
     display.textContent = displayVal;
